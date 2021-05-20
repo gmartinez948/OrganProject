bece8eb8cf0559852e1e825fcbdb237ddb86eb14
Jeremy Lewi <jeremy@lewi.us>
2021-05-20 14:42:16 -0700

    Latest.

--- templates/Organflsk.py
+++ templates/Organflsk.py
@@ -1,8 +1,11 @@
 '''
 Flask server calling the html code
 '''
-
+import random
 from flask import Flask, render_template
+
+def generate_data():
+    return "{0}".format(random.randint(0, 1000))
 
 app = Flask(__name__)
 @app.route("/")
@@ -11,7 +14,7 @@ def home():
         "top": "https://ufhealth.org/sites/default/files/media/social-media/2020-April-NDLM-2020-Organ-Donation.png",
         "middle": "https://lh3.googleusercontent.com/pw/ACtC-3flTYKdLYvr69_zSZyYBqDcI8DdoKnvVBcXqfS66akpOknrsl-io2AWviWBjdrcVDxOIsGuTog2oBteUJfTN94bG8Y07KH97p9HooB32v_lgxyPtYYJbO9i6Jb-vtMU_xTVPL5pOjhA8aPDTErlKVE=w388-h335-no?authuser=0",
     }
-    return render_template("index1.html", images=images, title="some title")
+    return render_template("index1.html", images=images, title="some title", data=generate_data())
 
 if __name__ == '__main__':
     app.run()
